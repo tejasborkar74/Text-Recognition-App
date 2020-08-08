32649915e94a77c1dbb8c4132c911ec982fc259e
unknown <tejasborkar74@gmail.com>
2020-08-09 01:47:34 +0530

    Finall touchs

--- app/src/main/java/com/example/textrecognition/MainActivity.java
+++ app/src/main/java/com/example/textrecognition/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
@@ -21,16 +22,20 @@ import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 import com.google.firebase.ml.vision.text.FirebaseVisionText;
 import com.google.firebase.ml.vision.text.FirebaseVisionTextDetector;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button captureButton , detectButton;
+    Button captureButton , detectButton , toGallery;
     ImageView imageView;
     TextView textView;
     ScrollView scrollView;
 
-    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    static final int FOR_GALLERY = 1 , FOR_CLICK = 5;
     Bitmap imageBitmap;
 
     @Override
@@ -40,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
 
         captureButton = findViewById(R.id.capture);
         detectButton = findViewById(R.id.detectText);
+        toGallery = findViewById(R.id.toGallery);
 
         imageView = findViewById(R.id.image);
         textView = findViewById(R.id.textView);
@@ -51,9 +57,18 @@ public class MainActivity extends AppCompatActivity {
             {
                 textView.setText("");
                 dispatchTakePictureIntent();
-
             }
         });
+
+        toGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText("");
+                picturefromgallery();
+            }
+        });
+
+
 
         detectButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -64,13 +79,21 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    void picturefromgallery()
+    {
+        Intent fromGallery = new Intent();
+        fromGallery.setType("image/*");
+        fromGallery.setAction(Intent.ACTION_GET_CONTENT);
 
+        startActivityForResult(Intent.createChooser(fromGallery , "Select Picture"),FOR_GALLERY);
+
+    }
 
 
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            startActivityForResult(takePictureIntent, FOR_CLICK);
         }
     }
 
@@ -78,7 +101,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+        if (requestCode == FOR_GALLERY && resultCode == RESULT_OK) {
+
+            try {
+                InputStream  inputStream = getContentResolver().openInputStream(data.getData());
+                imageBitmap = BitmapFactory.decodeStream(inputStream);
+                imageView.setImageBitmap(imageBitmap);
+            }catch (FileNotFoundException e)
+            {
+                e.printStackTrace();
+                Toast.makeText(MainActivity.this , "Error: "+ e.getMessage() , Toast.LENGTH_LONG).show();
+            }
+        }
+        else if(requestCode == FOR_CLICK && resultCode == RESULT_OK)
+        {
             Bundle extras = data.getExtras();
             imageBitmap = (Bitmap) extras.get("data");
             imageView.setImageBitmap(imageBitmap);
